9a3f102ffec3d8cf93879028c53a7a62f81e77fd
Rob Van Den Noortgate <56111698+VDN-Rob@users.noreply.github.com>
2026-08-08 21:54:42 +0200

    Experimented with React

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,13 @@
+import { MouseEventHandler, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    // alert('You clicked me!');
+    setCount(count + 1);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Cycling Companion</Text>
@@ -9,6 +16,8 @@ export default function Index() {
 
       <Text>No trips yet.</Text>
 
+      <Button title="New Trip" disabled={false} count={count}  onClick={handleClick}/>
+      <Button title="" disabled={false} count={count} onClick={handleClick}/>
       <Text>[ + New Trip ]</Text>
     </View>
   );
@@ -21,3 +30,23 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+
+// Button React component
+interface ButtonProps {
+  /** Text displayed inside the button */
+  title: string;
+  /** Whether the button can be interacted with */
+  disabled: boolean;
+
+  count: number;
+  onClick: MouseEventHandler;
+}
+
+export function Button({ title, disabled, count, onClick}: ButtonProps) {
+  
+
+  return (
+    <button disabled={disabled} onClick={onClick}> {title} Clicked: {count} </button>
+  );
+}
