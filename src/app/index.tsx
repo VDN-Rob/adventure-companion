6ef7144f706b4a195443026fca5c1df03d70f3ed
Rob Van Den Noortgate <56111698+VDN-Rob@users.noreply.github.com>
2026-08-09 16:18:52 +0200

    Added a working button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,52 +1,75 @@
-import { MouseEventHandler, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Trip } from "@/models/Trip";
+import { useState } from "react";
+import { Button, SafeAreaView, StyleSheet, Text } from "react-native";
 
 export default function Index() {
+  // Temporary memory
+  const [trips, setTrips] = useState<Trip[]>([]);
+  const [text, onChangeText] = useState('Useless Text');
   const [count, setCount] = useState(0);
-  function handleClick() {
-    // alert('You clicked me!');
-    setCount(count + 1);
+
+  // Event handlers
+  function handleAddTrip() {
+    const newTrip: Trip = {
+      id: "0", // TODO
+      name: "Test", // TODO
+      startDate: "01.01.00", // TODO
+      endDate: "31.12.00" // TODO
+    }
+    setTrips([...trips, newTrip]);
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Cycling Companion</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Cycling Companion</Text>
 
       <Text>Your offline cycling companion.</Text>
-
-      <Text>No trips yet.</Text>
-
-      <Button title="New Trip" disabled={false} count={count}  onClick={handleClick}/>
-      <Button title="" disabled={false} count={count} onClick={handleClick}/>
-      <Text>[ + New Trip ]</Text>
-    </View>
+      
+      {trips.length === 0 ? (
+        <Text>No trips yet.</Text>
+      ) : (
+        trips.map((trip) => (
+          <Text key={trip.id}>
+            {trip.name}
+            </Text> ))
+      )}
+      
+      <Button title="+ New Trip" onPress={handleAddTrip} />
+    </SafeAreaView>
   );
 }
 
+// Button list component
+type ItemProps = { title: string };
+
+const Item = ({ title }: ItemProps) => (
+  <Text>{title}</Text>
+);
+
+
+
+
+
+// Aestethic
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },  
+    input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
 });
-
-
-// Button React component
-interface ButtonProps {
-  /** Text displayed inside the button */
-  title: string;
-  /** Whether the button can be interacted with */
-  disabled: boolean;
-
-  count: number;
-  onClick: MouseEventHandler;
-}
-
-export function Button({ title, disabled, count, onClick}: ButtonProps) {
-  
-
-  return (
-    <button disabled={disabled} onClick={onClick}> {title} Clicked: {count} </button>
-  );
-}
